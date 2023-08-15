c771b7e06111ea7540288d5b94395912331f7803
Marcia0420 <138834592+Marcia0420@users.noreply.github.com>
2023-08-15 20:15:52 +0000

    spotify

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,43 @@
-import React from "react";
-
+import React, { useState, useEffect, useRef } from "react";
+//  import hooks
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
+
+	//variable array vacio
+	const [myMusic, setMyMusic] = useState([])
+
+	//try catch
+	async function obtenerInfo() {
+		try {
+			const respuesta = await fetch("https://playground.4geeks.com/apis/fake/sound/songs")
+			const datos = await respuesta.json()
+			setMyMusic(datos)
+			console.log(datos)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+
+	useEffect(() => {
+		obtenerInfo()
+	}, [])
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+			<h1 className="text-center mt-5">Spotify</h1>
+			<ul className="list-group list-group-flush">{
+				myMusic.map((deaUna) =>{ 
+					return <li onClick={()=>{}} className="list-group-item">{deaUna.name}</li>	
+				})
+			}</ul>
+			<audio/>
+		</div >
 	);
 };
 
