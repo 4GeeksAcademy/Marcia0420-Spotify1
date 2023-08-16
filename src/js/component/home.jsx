daf060e4dd5cfe78ac80ec5c231a7936c3b23486
Marcia0420 <138834592+Marcia0420@users.noreply.github.com>
2023-08-16 13:32:26 +0000

    myMusic

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,7 +8,9 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 
 	//variable array vacio
-	const [myMusic, setMyMusic] = useState([])
+	const [myMusic, setMyMusic] = useState([]) //arreglo
+	const [musica, setMusica] = useState("")// string
+	const myAudio = useRef()
 
 	//try catch
 	async function obtenerInfo() {
@@ -23,6 +25,21 @@ const Home = () => {
 	}
 
 
+	function tocarMusica() {
+
+		myAudio.current.play()
+	}
+
+function pausarMusica(){
+
+	myAudio.current.pause()
+}
+
+
+
+
+
+
 	useEffect(() => {
 		obtenerInfo()
 	}, [])
@@ -32,11 +49,18 @@ const Home = () => {
 		<div className="text-center">
 			<h1 className="text-center mt-5">Spotify</h1>
 			<ul className="list-group list-group-flush">{
-				myMusic.map((deaUna) =>{ 
-					return <li onClick={()=>{}} className="list-group-item">{deaUna.name}</li>	
+				myMusic.map((deaUna) => {
+					return <li onClick={() => { setMusica(deaUna.url) }} className="list-group-item">{deaUna.id}- {deaUna.name}</li>
+
 				})
 			}</ul>
-			<audio/>
+			<footer className="d-flex justify-content-around">
+				<audio src={"https://assets.breatheco.de/apis/sound/" + musica} ref={myAudio}></audio>
+				<i className="fa fa-backward" style={{ color: "#00040a" }}></i>
+				<i onClick={() => { tocarMusica() }} className="fa fa-play" style={{ color: "#00060f" }}></i>
+				<i onClick={() => { pausarMusica() }} className="fa fa-pause" style={{ color: " #010409" }}></i>
+				<i className="fa fa-forward" style={{ color: "#000205" }}></i>
+			</footer>
 		</div >
 	);
 };
